6579ba7faed3b82c49a0b09a549bb4a06ba6cf34
Plus <glebovglebov@gmail.com>
2020-04-09 19:00:15 +0700

    First commit

---
+++ Assets/Plugins/PerlinNoise2D/Float3.cs
@@ -0,0 +1,48 @@
+﻿namespace Plus.Noise
+{
+    public struct QuadVectors
+    {
+        public Float3 bottomLeft { get; }
+        public Float3 bottomRight { get; }
+        public Float3 topLeft { get; }
+        public Float3 topRight { get; }
+
+        public QuadVectors(Float3 bottomLeft, Float3 bottomRight, Float3 topLeft, Float3 topRight)
+        {
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+        }
+    }
+
+    public struct Float3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Float3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static float Dot(Float3 first, Float3 second)
+        {
+            return (first.x * second.x + first.y * second.y + first.z * second.z);
+        }
+
+        public static Float3 operator +(Float3 first, Float3 second)
+        {
+            return new Float3(first.x + second.x, first.y + second.y, first.z + second.z);
+        }
+
+        public static Float3 operator -(Float3 first, Float3 second)
+        {
+            return new Float3(first.x - second.x, first.y - second.y, first.z - second.z);
+        }
+    }
+}
+
---
+++ Makefile
@@ -0,0 +1,2 @@
+deploy:
+	git subtree push --prefix Assets/Plugins/PerlinNoise2D origin upm
